785af744aaed431bd2fbb78c9d5a22b018c3b4fc
Algol0x7DB <algol0x7db@gmx.com>
2013-08-22 12:35:09 +0200

    Update WidgetTest.java

--- WidgetTest.java
+++ WidgetTest.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.algol.widgettest;
 
 import java.awt.Color;
 import java.awt.Dimension;
